390265d69171a4ea076e85e15c67535c92ce186c
chinux_2012 <1872326010@qq.com>
2021-03-10 16:47:34 +0800

    all

--- reactClient/cloudshare-react/src/content/pubComponent/usedToSlots/window/window.js
+++ reactClient/cloudshare-react/src/content/pubComponent/usedToSlots/window/window.js
@@ -15,6 +15,8 @@ class Window extends Component {
         this.containerRef = null
 
         this.handle_container_mouseDown = this.handle_container_mouseDown.bind(this)
+        this.handle_container_mouseMove = this.handle_container_mouseMove.bind(this)
+        this.handle_container_mouseUp   = this.handle_container_mouseUp.bind(this)
     }
 
     handle_container_mouseDown (e) {
@@ -23,16 +25,28 @@ class Window extends Component {
         let dom_W   = e.target.offsetWidth
         let dom_H   = e.target.offsetHeight
         console.log(`mouse_X:${mouse_X} mouse_Y:${mouse_Y} dom_W:${dom_W} dom_H:${dom_H}`, this.containerRef.style)
-        this.containerRef.width = 300   
+        this.setState({ win_W: 400, win_H: 400 })
+    }
+    handle_container_mouseMove (e) {
+
+    }
+    handle_container_mouseUp (e) {
+
     }
 
     componentDidMount () {
+        // 窗口绑定事件
         this.containerRef.onmousedown = this.handle_container_mouseDown
+        this.containerRef.onmousemove = this.handle_container_mouseMove
+        this.containerRef.onmouseup   = this.handle_container_mouseUp
     }
 
     render () {
         return (
-            <div ref={(ref) => {this.containerRef = ref}} className="window_container">
+            <div ref={(ref) => {this.containerRef = ref}} className="window_container" style={{
+                width:  this.state.win_W + 'px',
+                height: this.state.win_H + 'px'
+            }}>
                 <div className="window_header">
                     <span className="window_title">
                         
